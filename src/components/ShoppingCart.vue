3a689c83c2e3d9d7a188146df895b167ab8ec4d5
Priit Pärnpuu <iampriit@gmail.com>
2024-11-22 16:22:40 +0200

    Vue.js, Node.js

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,33 @@
+<template>
+<h1 class="text-center">Cart Total</h1>
+  <h3>Total:</h3><h3>${{ cartTotal.toFixed(2)}}</h3>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data:() => ({
+    apibase: "http://localhost:8080/api/cart",
+    cartTotal:0
+  }),
+  mounted(){
+    axios
+        .get(`${this.apibase}/cart-total`)
+        .then((res) => (this.cartTotal = res.data))
+        .catch(console.error)
+  }
+}
+
+</script>
+
+
+
+
+
+
+
+<style>
+
+</style>
+
+
